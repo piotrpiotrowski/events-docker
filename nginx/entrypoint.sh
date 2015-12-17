0c571116abfd38c7f2a2e5afde130de3a1da0ef5
piotrpiotrowski <piotrpiotrowski87@gmail.com>
2015-12-17 22:25:48 +0100

    [EVN-0027] create cors support for docker environment - integration tests passed

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
-export host_ip=`/sbin/ip route|awk '/default/ { print $3 }'`
-cd /usr/share/nginx/html/js/ && envsubst '$api_url$host_ip' < main.js > temporaryMain.js && mv temporaryMain.js main.js
+echo "siema"
+
+cd /usr/share/nginx/html/js/ 
+envsubst '$api_url' < main.js > temporaryMain.js && mv temporaryMain.js main.js
+
+export host_ip=`/sbin/ip route|awk '/default/ { print $3 }'` 
+envsubst '$host_ip' < main.js > temporaryMain.js && mv temporaryMain.js main.js
 
 exec "$@"
