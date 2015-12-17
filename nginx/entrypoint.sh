03fde7d9e1d4c82a920e5d873c274809f40cf74e
piotrpiotrowski <piotrpiotrowski87@gmail.com>
2015-12-17 19:07:29 +0100

    [EVN-0027] create 'cors support for docker environment - extract host_ip inside of container

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-
-cd /usr/share/nginx/html/js/ && envsubst '$API_URL' < main.js > temporaryMain.js && mv temporaryMain.js main.js
+export host_ip=`/sbin/ip route|awk '/default/ { print $3 }'`
+cd /usr/share/nginx/html/js/ && envsubst '$api_url$host_ip' < main.js > temporaryMain.js && mv temporaryMain.js main.js
 
 exec "$@"
