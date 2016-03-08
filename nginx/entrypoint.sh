f6ca271981d48b03ee6ce25d3d9dcb1d058a9a88
piotrpiotrowski <piotrpiotrowski87@gmail.com>
2016-03-08 20:53:34 +0100

    [EVN-0027] create cors support for docker environment - single environment variable for api_url

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 cd /usr/share/nginx/html/js/ 
 
 export host_ip=`/sbin/ip route|awk '/default/ { print $3 }'` 
-envsubst '$protocol $host_ip $host_port $api_url' < main.js > temporaryMain.js && mv temporaryMain.js main.js
+envsubst '$api_url' < main.js > temporaryMain.js && mv temporaryMain.js main.js
 
 exec "$@"
